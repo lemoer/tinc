54b8bc6e86cd88d310d6bfde4ccf020aeea67bed
thorkill <thkr@hannover.ccc.de>
2015-06-30 18:10:18 +0200

    Revert "Type mismatch and debug_t is always >= 0"
    This reverts commit 62dc7b6fe547a4f1a5cca669a177f3ea668c8186.

--- src/control.c
+++ src/control.c
@@ -78,11 +78,12 @@ bool control_h(connection_t *c, const char *request) {
 			return control_ok(c, REQ_PURGE);
 
 		case REQ_SET_DEBUG: {
-			debug_t new_level;
+			int new_level;
 			if(sscanf(request, "%*d %*d %d", &new_level) != 1)
 				return false;
 			send_request(c, "%d %d %d", CONTROL, REQ_SET_DEBUG, debug_level);
-			debug_level = new_level;
+			if(new_level >= 0)
+				debug_level = new_level;
 			return true;
 		}
 
