d803ac93dc8a48dcd8a4d97f3e90767a3512d3d3
thorkill <thkr@hannover.ccc.de>
2015-06-30 19:23:15 +0200

    Initialize variables in netutl.c

--- src/netutl.c
+++ src/netutl.c
@@ -33,8 +33,10 @@ bool hostnames = false;
   Return NULL on failure.
 */
 struct addrinfo *str2addrinfo(const char *address, const char *service, int socktype) {
-	struct addrinfo *ai, hint = {0};
+	struct addrinfo *ai, hint;
 	int err;
+
+	memset(&hint, 0x0, sizeof(struct addrinfo));
 
 	hint.ai_family = addressfamily;
 	hint.ai_socktype = socktype;
@@ -53,9 +55,12 @@ struct addrinfo *str2addrinfo(const char *address, const char *service, int sock
 }
 
 sockaddr_t str2sockaddr(const char *address, const char *port) {
-	struct addrinfo *ai, hint = {0};
-	sockaddr_t result = {{0}};
+	struct addrinfo *ai, hint;
+	sockaddr_t result;
 	int err;
+
+	memset(&hint, 0x0, sizeof(struct addrinfo));
+	memset(&result, 0x0, sizeof(sockaddr_t));
 
 	hint.ai_family = AF_UNSPEC;
 	hint.ai_flags = AI_NUMERICHOST;
